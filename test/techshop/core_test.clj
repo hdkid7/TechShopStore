5d06a534f432afbb716f907de043c57d4a600180
mrstandoff <git@bridgepattern.com>
2020-12-26 12:10:39 -0600

    nil product and requires name property

--- test/techshop/core_test.clj
+++ test/techshop/core_test.clj
@@ -2,6 +2,19 @@
   (:require [clojure.test :refer :all]
             [techshop.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 1 1))))
+(defn create-product [arg] (
+  (if (= arg nil) (throw (NullPointerException.))
+    (if (not (:name arg)) (throw (Exception. "requires name property")))
+  )
+))
+
+
+(deftest product-manager-suite
+  (testing "cannot create nil product"
+    (is (thrown? NullPointerException (create-product nil)) )
+  )
+  (testing "requires a name property"
+    (is (thrown-with-msg? Exception #"requires name property"
+                          (create-product {:notaname ""})))
+  )
+)
