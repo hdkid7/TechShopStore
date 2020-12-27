f2d227d34881e931ebae6dd1b439e714f0b21061
mrstandoff <git@bridgepattern.com>
2020-12-26 18:04:21 -0600

    SaveProductCommand

--- test/techshop/core_test.clj
+++ test/techshop/core_test.clj
@@ -2,19 +2,53 @@
   (:require [clojure.test :refer :all]
             [techshop.core :refer :all]))
 
-(defn create-product [arg] (
-  (if (= arg nil) (throw (NullPointerException.))
-    (if (not (:name arg)) (throw (Exception. "requires name property")))
-  )
-))
+(defprotocol ProductRepository
+  (save [this product]))
 
+(def saved-product (atom nil))
+(deftype InMemoryProductRepository []
+  ProductRepository
+  (save [this product] (swap! saved-product (fn [] product))))
 
-(deftest product-manager-suite
-  (testing "cannot create nil product"
-    (is (thrown? NullPointerException (create-product nil)) )
-  )
-  (testing "requires a name property"
-    (is (thrown-with-msg? Exception #"requires name property"
-                          (create-product {:notaname ""})))
+(defprotocol ProductSaver
+  (save-product [this record]))
+
+(deftype SaveProductCommand [product-repository]
+  ProductSaver
+  (save-product [this record] (
+      (if (not (map? record)) (throw (Exception. "product must be a map")))
+      (if (empty? record) (throw (Exception. "argument is empty")))
+      (if (not (:name record)) (throw (Exception. "missing name property")))
+      (if (empty? (:name record)) (throw (Exception. "name is empty")))
+  )))
+
+(deftest save-product-suite
+  (let [cmd (SaveProductCommand. (InMemoryProductRepository.)) ]
+    (testing "product must be a map"
+      (let [regex #"product must be a map"]
+        (is (thrown-with-msg? Exception regex (save-product cmd nil)))
+        (is (thrown-with-msg? Exception regex (save-product cmd "")))
+        (is (thrown-with-msg? Exception regex (save-product cmd [])))
+      )
+    )
+    (testing "throws on empty argument"
+      (is (thrown-with-msg? Exception #"argument is empty" (save-product cmd {})))
+    )
+    (testing "throws if arg missing name property"
+      (is (thrown-with-msg? Exception #"missing name property"
+                            (save-product cmd {:stock 5})))
+    )
+    (testing "throws if name is empty"
+      (is (thrown-with-msg? Exception #"name is empty"
+                            (save-product cmd {:name "" :stock 5})))
+      )
+    ;(testing "name but no stock, leaves stock as 0 by default"
+    ;
+    ;  (let [cmd (SaveProductCommand. (SpyProductRepo.))] (is
+    ;     (do
+    ;       (save-product cmd {:name "Macaroni"})
+    ;       )
+    ;     ))
+    ;)
   )
 )
